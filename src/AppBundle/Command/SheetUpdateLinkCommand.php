ee95d7dc809583fdac7ae606b97487be28916f07
Simon Minotto <simon.minotto@gmail.com>
2016-12-19 20:32:53 +0100

    Gestion des programmes d'affiliation

---
+++ src/AppBundle/Command/SheetUpdateLinkCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+
+
+class SheetUpdateLinkCommand extends ContainerAwareCommand
+{
+
+    
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $startTime = time();
+        $this->input = $input;
+        $this->output = $output;
+        //$ecomPriceSer = $this->getContainer()->get('ecom.price');
+        $ecomAffiSer = $this->getContainer()->get('ecom.affiliation');
+        $ecomSheet = $this->getContainer()->get('ecom.sheet_updater');
+
+        $docId = $this->input->getOption('doc');
+
+        $letters='abcdefghijklmnopqrstuvwxyz';
+        $columnForUrl=null;
+        for($iColumn=0;$iColumn<24;$iColumn++)
+        {
+            $letter = $letters{$iColumn};
+            $value = $ecomSheet->getSheetValue($docId, $letter."1");
+            if($value == "Lien pour acheter")
+            {
+                $columnForUrl = $letter;
+            }
+            if($columnForUrl)
+            {
+                break;
+            }
+        }
+        if(empty($columnForUrl))
+        {
+            throw new \Exception("Unable to find url column");
+        }
+        
+        $maxLineNumber = 100;
+        $nbErrorLeft=10;
+        for($lineNumber=2;$lineNumber<=$maxLineNumber;$lineNumber++)
+        {
+            $url = $ecomSheet->getSheetValue($docId, $columnForUrl.$lineNumber);
+            if(empty($url))
+            {
+                $nbErrorLeft--;
+                if($nbErrorLeft>=0)
+                {
+                    continue;
+                }else
+                {
+                    break;
+                }
+            }
+            $newUrl = $ecomAffiSer->getNewUrl($url);
+            if($newUrl != $url)
+            {
+                $this->output->writeln('' . $url . ' => ' . $newUrl.'');
+                //$ecomSheet->setSheetValue($docId, $columnForUrl.$lineNumber, $newUrl);
+            }
+        }
+        
+        $this->output->writeln("DONE");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->setName('ecomscraper:sheet:updatelink');
+        $this->setDescription('Update links on sheets');
+
+
+        $this->addOption(
+          'doc',
+          'd',
+          InputOption::VALUE_REQUIRED,
+          'Document segment',
+          null
+        );
+        
+    }
+}
---
+++ src/AppBundle/Service/EcomAffiliation.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Service;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+
+
+class EcomAffiliation
+{
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getNewUrl($url)
+    {
+        $hostname = parse_url($url, PHP_URL_HOST);
+        $hostname = strtolower($hostname);
+        switch($hostname)
+        {
+            case 'www.boulanger.fr':
+            case 'www.boulanger.com':
+                return $this->getAffiliationForBoulanger($url);
+
+            case 'www.amazon.fr':
+            case 'www.amazon.com':
+                return $this->getAffiliationForAmazon($url);
+        }
+        return $url;
+    }
+
+    protected function getAffiliationForBoulanger($url)
+    {
+        return "http://track.effiliation.com/servlet/effi.redir?id_compteur=16300285&url=".urlencode($url);
+    }
+
+    protected function getAffiliationForAmazon($url)
+    {
+        $currentTag = null;
+        $actualQueryString = parse_url($url, PHP_URL_QUERY);
+        $actualQueryStringParameters = array();
+        parse_str($actualQueryString, $actualQueryStringParameters);
+        $actualQueryStringParameters['tag'] = 'labellenergie-21';
+        
+        $tmp = explode('?', $url);
+        
+        return $tmp[0].'?'.http_build_query($actualQueryStringParameters, null, '&');
+    }
+}
